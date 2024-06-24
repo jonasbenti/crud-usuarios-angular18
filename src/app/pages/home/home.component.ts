249fb9edaee51073ce2c5c76b88b9d0c935119f4
Jonas <jonasbenti@hotmail.com>
2024-06-23 22:32:37 -0300

    Criar componente tela inicial/dashboard

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  userName: string | null;
+
+  ngOnInit() {
+    this.userName = sessionStorage.getItem('user');
+  }
+}
