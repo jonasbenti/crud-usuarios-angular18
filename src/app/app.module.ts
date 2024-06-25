149d3ae4fbd5619fc74a3f61c800c5040cbfc82b
Jonas <jonasbenti@hotmail.com>
2024-06-24 21:59:23 -0300

    Adicionar firebase ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -12,6 +12,20 @@ import { ButtonComponent } from './components/button/button.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
+import { CrudComponent } from './pages/crud/crud.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatSortModule} from '@angular/material/sort';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AngularFireModule } from '@angular/fire/compat';
+
 
 
 @NgModule({
@@ -20,17 +34,30 @@ import { MenuComponent } from './components/menu/menu.component';
     ButtonComponent,
     LoginComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    // ANGULAR MATERIAL
     MatIconModule,
-    MatProgressSpinnerModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    // provideFirebaseApp(() => initializeApp({"projectId":"crud-usuarios-angular-3a135","appId":"1:933535996206:web:b1a435a4dd0c76380b42bf","storageBucket":"crud-usuarios-angular-3a135.appspot.com","apiKey":"AIzaSyA6PrR4K3Qc0lH0hHL6gIAyxG8Ch6fSgfE","authDomain":"crud-usuarios-angular-3a135.firebaseapp.com","messagingSenderId":"933535996206"})),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: " sua apiKey",
+        authDomain: "seu authDomain",
+        projectId: "seu projectId",
+        storageBucket: "sua storageBucket",
+        messagingSenderId: "seu messagingSenderId",
+        appId: "sua appId"
+    }
+};
